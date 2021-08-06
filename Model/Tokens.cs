bc420f74a16ed9c46b66c8607ca3f9975fbf082e
Sumit Sharma <sumittechie@outlook.com>
2021-08-06 10:29:14 +0530

    Added view models and model into model project

---
+++ Model/Tokens.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Tokens
+    {
+
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public string UserId { get; set; }
+        public DateTime ExpiryOn { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedByIp { get; set; }
+        public DateTime RevokedOn { get; set; }
+        public string RevokedByIp { get; set; }
+    }
+}
---
+++ Model/ViewModels/UserDetailsVm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModels
+{
+    public class UserDetailsVm
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Mobile { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
